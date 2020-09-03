741979947bd879f7a056f7d56105aab637d487d1
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-09-03 05:43:05 +0000

    Automatically backed up by Learn

--- greeting.rb
+++ greeting.rb
@@ -1,14 +1,3 @@
-def say_hello_world_ten_times
-  phrase = "Hello World"
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
+def greeting
+  puts "Hello World"
 end
-say_hello_world_ten_times
