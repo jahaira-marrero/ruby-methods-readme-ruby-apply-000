fbc44f42b39845e33bbb2cdac9984738c9b5692e
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-09-03 05:36:06 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,15 +1,2 @@
-irb
 def say_hello_world_ten_times
-  phrase = "Hello World!"
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-end
-say_hello_world_ten_times
+  phrase = "Hello World
